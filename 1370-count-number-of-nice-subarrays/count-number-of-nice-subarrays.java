265f927fcdd41fd3118c1a0c8748276a8805d1be
rahulsharma2005 <144013694+rahulsharma2005@users.noreply.github.com>
2026-09-09 08:58:57 +0530

    Time: 45 ms (12.09%) | Memory: 56.1 MB (91.32%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -2,14 +2,13 @@ class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         HashMap<Integer,Integer> mp = new HashMap<>();
         mp.put(0,1);
-        int count = 0,ans = 0;
-        for(int i = 0 ; i < nums.length; i++){
+        int n = nums.length,count = 0 , ans = 0;
+        for(int  i = 0 ; i < n ; i++){
             if(nums[i] % 2 == 1) count++;
-
             if(mp.containsKey(count - k)){
-                ans += mp.get(count - k); 
+                ans += mp.get(count-k);
             }
-            mp.put(count,mp.getOrDefault(count,0)+1);
+            mp.put(count , mp.getOrDefault(count , 0)+1);
         }
         return ans;
     }
