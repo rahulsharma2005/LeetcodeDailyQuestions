c525b657694ad8b2140efb920747ad2b54f620ab
rahulsharma2005 <144013694+rahulsharma2005@users.noreply.github.com>
2026-09-25 19:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (50.35%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -5,9 +5,9 @@ class Solution {
             while(!strs[i].startsWith(prefix)){
                 prefix = prefix.substring(0,prefix.length()-1);
 
-                if(prefix.isEmpty()){
-                    return "";
-                }
+                // if(prefix.isEmpty()){
+                //     return "";
+                // }
             }
         }
         return prefix;
